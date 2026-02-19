6c93e4ae1e226c8793cb79601cbb951f4724abe2
rainsdm <59646708+rainsdm@users.noreply.github.com>
2026-02-19 21:06:11 +0800

    全新重构的初始化提交。

---
+++ src/main/java/cn/snowdreamsblog/redCulture/RedCultureApplication.java
@@ -0,0 +1,13 @@
+package cn.snowdreamsblog.redCulture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedCultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/snowdreamsblog/redCulture/RedCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.snowdreamsblog.redCulture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
