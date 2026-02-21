a99c6de2548e9e154f44cbed40489ce88f9e892c
rainsdm <59646708+rainsdm@users.noreply.github.com>
2026-02-21 19:04:40 +0800

    添加了静态的站点页面。

--- src/main/java/cn/snowdreamsblog/redCulture/RedCultureApplication.java
+++ src/main/java/cn/snowdreamsblog/redCulture/RedCultureApplication.java
@@ -2,8 +2,9 @@ package cn.snowdreamsblog.redCulture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RedCultureApplication {
 
 	public static void main(String[] args) {
